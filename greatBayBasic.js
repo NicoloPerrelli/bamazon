53958b52b611e400e6112060d8dc56b51e42ec19
Nicolo Perrelli <nicoloperrelli@gmail.com>
2018-12-14 11:18:06 -0500

    post2

--- greatBayBasic.js
+++ greatBayBasic.js
@@ -25,68 +25,55 @@ function loadProducts() {
 	});
  }
 
-// function which prompts the user for what action they should take
-function start() {
-
-	inquirer
-		.prompt({
-			name: "postOrBid",
-			type: "rawlist",
-			message: "Would you like to [POST] an auction or [BID] on an auction?",
-			choices: ["POST", "BID"]
-		})
-		.then(function(answer) {
-			// based on their answer, either call the bid or the post functions
-			if (answer.postOrBid.toUpperCase() === "POST") {postAuction();}
-			else {bidAuction();}
-		});
+function start() {//start asking what to do
+	inquirer.prompt({
+		name: "postOrBid",
+		type: "rawlist",
+		message: "Would you like to [POST] an auction or [BID] on an auction?",
+		choices: ["POST", "BID"]//one or two
+	}).then(function(answer) {//waiting for response. Then branch.
+		if (answer.postOrBid.toUpperCase() === "POST") {postAuction();}
+		else {bidAuction();}
+	});
 }
 
-// function to handle posting new items up for auction
 function postAuction() {
-	// prompt for info about the item being put up for auction
-	inquirer
-		.prompt([
-			{
-				name: "item",
-				type: "input",
-				message: "What is the item you would like to submit?"
-			},
-			{
-				name: "category",
-				type: "input",
-				message: "What category would you like to place your auction in?"
-			},
-			{
-				name: "startingBid",
-				type: "input",
-				message: "What would you like your starting bid to be?",
-				validate: function(value) {
-					if (isNaN(value) === false) {
-						return true;
-					}
-					return false;
-				}
+	inquirer.prompt([
+		{
+			name: "item",
+			type: "input",
+			message: "What is the item you would like to submit?"
+		},
+		{
+			name: "category",
+			type: "input",
+			message: "What category would you like to place your auction in?"
+		},
+		{
+			name: "startingBid",
+			type: "input",
+			message: "What would you like your starting bid to be?",
+			validate: function(value) {
+				if (isNaN(value) === false) {return true;}
+				return false;
 			}
-		])
-		.then(function(answer) {
-			// when finished prompting, insert a new item into the db with that info
-			connection.query(
-				"INSERT INTO auctions SET ?",
-				{
-					item_name: answer.item,
-					category: answer.category,
-					starting_bid: answer.startingBid,
-					highest_bid: answer.startingBid
-				},
-				function(err) {
-					if (err) throw err;
-					console.log("Your auction was created successfully!");
-					// re-prompt the user for if they want to bid or post
-					start();
-				}
-			);
-		});
+		}
+	]).then(function(answer) {
+		connection.query(
+			"INSERT INTO auctions SET ?",
+			{
+				item_name: answer.item,
+				category: answer.category,
+				starting_bid: answer.startingBid,
+				highest_bid: answer.startingBid
+			},
+			function(err) {
+				if (err) throw err;
+				console.log("Your auction was created successfully!");
+				start();//back to start
+			}
+		);
+	});
 }
 
 function bidAuction() {
